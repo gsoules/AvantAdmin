1b67f861f259a9fd4b53420ef7bfabe468da830c
gsoules <gsoules@avantlogic.com>
2018-06-22 16:58:09 -0400

    Add a separator between Dublin Core and Item Metadata fields.

--- AvantAdminPlugin.php
+++ AvantAdminPlugin.php
@@ -29,8 +29,9 @@ class AvantAdminPlugin extends Omeka_Plugin_AbstractPlugin
         $images = item_image_gallery(array('linkWrapper' => array('class' => 'admin-thumb panel'), 'link' => array('target' => '_blank')), 'thumbnail', false);
 
         // Merge the contents of the first two tabs into a single tab that shows the item's image(s) at the top.
+        $fieldsSeparator = '<div class="field field-separator">' . get_option('avantadmin_type_name') . ' ' . __('Fields') . '</div>';
         $newTabs = array();
-        $newTabs[__('Fields')] = $images . $tabs['Dublin Core'] . $tabs['Item Type Metadata'];
+        $newTabs[__('Fields')] = $images . $tabs['Dublin Core'] . $fieldsSeparator . $tabs['Item Type Metadata'];
 
         // Append the remaining tabs.
         foreach ($tabs as $key => $tab)
