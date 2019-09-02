0fbf157a08ccc2944e81b684bd21bfaee02e2351
gsoules <gsoules@avantlogic.com>
2019-09-02 11:21:25 -0400

    Intermediate support for Relationships page.

--- views/admin/index/relationships.php
+++ views/admin/index/relationships.php
@@ -16,70 +16,51 @@ if ($itemTitle != '' && $itemTitle != __('[Untitled]')) {
 
 $itemTitle = __('Relationships for Item %s', ItemMetadata::getItemIdentifier($item));
 
-echo head(array('title' => $itemTitle, 'bodyclass'=>'items show'));
-echo flash();
+echo head(array('title' => $itemTitle, 'bodyclass'=>'relationships'));
+//echo flash();
 ?>
 
-<section class="seven columns alpha">
-    <?php echo flash(); ?>
-    <?php
-    echo item_image_gallery(
-        array('linkWrapper' => array('class' => 'admin-thumb panel'),
-            'link' => array('target' => '_blank')), 'thumbnail', false);
-    ?>
+<!--<section class="seven columns alpha">-->
+<!--    --><?php
+    //echo flash();
 
-    <?php
-    $title = ItemMetadata::getItemTitle($item);
     $type = ItemMetadata::getElementTextForElementName($item, 'Type');
     $subject = ItemMetadata::getElementTextForElementName($item, 'Subject');
-    echo $title;
-    echo $type;
-    echo $subject;
+
+    $html = '';
+    $class = '';
+    $imageUrl = ItemPreview::getImageUrl($item, true, true);
+    $html .= "<img $class src='$imageUrl'>";
+
 
     $itemPreview = new ItemPreview($item);
-    echo $itemPreview->emitItemPreview();
+//    $html = '<div class="item-preview relationships-image">';
+//    $html .= $itemPreview->emitItemThumbnail();
+//    $html .= '</div>';
+//    $html .= '<div class="relationships-metadata">';
+    $html .= $itemPreview->emitItemHeader();
+    $html .= $itemPreview->emitItemTitle();
+    $html .= "<div>Type: $type</div>";
+    $html .= "<div>Subject: $subject</div>";
+
+    echo $html;
     ?>
-</section>
-
-<section class="three columns omega">
-    <ul class="pagination">
-        <?php if (($prevLink = link_to_previous_item_show(__('Prev Item')))): ?>
-        <li id="previous-item" class="previous">
-            <?php echo $prevLink; ?>
-        </li>
-        <?php endif; ?>
-        <?php if (($nextLink = link_to_next_item_show(__('Next Item')))): ?>
-        <li id="next-item" class="next">
-            <?php echo $nextLink; ?>
-        </li>
-        <?php endif; ?>
-    </ul>
-
-    <div id="edit" class="panel">
-<!--        --><?php //if (is_allowed($item, 'edit')): ?>
-<!--        --><?php
-//        echo link_to_item(__('Edit'), array('class'=>'big green button'), 'edit'); ?>
-<!--        --><?php //endif; ?>
-        <a href="<?php echo html_escape(public_url('items/show/'.metadata('item', 'id'))); ?>" class="big blue button" target="_blank"><?php echo __('View Public Page'); ?></a>
-<!--        --><?php //if (is_allowed($item, 'delete')): ?>
-<!--        --><?php //echo link_to_item(__('Delete This Item'), array('class' => 'delete-confirm big red button'), 'delete-confirm'); ?>
-<!--        <a href="--><?php //echo html_escape(admin_url('items/add/')); ?><!--" class="big blue button" target="_blank">--><?php //echo __('Add New Item'); ?><!--</a>-->
-<!--        --><?php //endif; ?>
-    </div>
-
-    <?php
-    echo get_specific_plugin_hook_output('AvantRelationships', 'show_relationships_visualization', array('view' => $this, 'item' => $item));
-    ?>
-</section>
+<!--</section>-->
+<!---->
+<!--<section class="three columns omega">-->
+<!--    <div id="edit" class="panel">-->
+<!--        <a href="--><?php //echo html_escape(public_url('items/show/'.metadata('item', 'id'))); ?><!--" class="big blue button" target="_blank">--><?php //echo __('View Public Page'); ?><!--</a>-->
+<!--    </div>-->
+<!---->
+<!--    --><?php
+//    echo get_specific_plugin_hook_output('AvantRelationships', 'show_relationships_visualization', array('view' => $this, 'item' => $item));
+//    ?>
+<!--</section>-->
 
 <?php
 $primaryItemIdentifier = ItemMetadata::getItemIdentifier($item);
 ?>
-<div><?php echo ItemMetadata::getItemTitle($item); ?></div>
-<br/>
-<div>This item is: <?php echo $primaryItemIdentifier; ?></div>
-<br/>
-<table>
+<table class="relationships-table">
     <thead>
     <tr>
         <th class="relationship-table-relationship"><?php echo __('Relationship'); ?></th>
