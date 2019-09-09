1993642f1598b6ec51c4099ad5962347ce249af8
gsoules <gsoules@avantlogic.com>
2019-09-09 18:14:02 -0400

    Add stats to the dashboard.

--- views/public/index/dashboard.php
+++ views/public/index/dashboard.php
@@ -2,21 +2,38 @@
 $pageTitle = __('Dashboard');
 echo head(array('bodyclass' => 'index primary-secondary', 'title' => $pageTitle));
 
-$total_items = total_records('Item');
-$stats = array(link_to('items', null, $total_items), __(plural('item', 'items', $total_items)));
+$stats = '';
+$useElasticsearch = AvantSearch::useElasticsearch();
+if ($useElasticsearch)
+{
+    $indexName = AvantElasticsearch::getNameOfLocalIndex();
+    $stats = AvantElasticsearch::generateContributorStatistics($indexName);
+}
 
-$html = "<p>The Digital Archive contains $stats[0] $stats[1]</p>";
+$html = '<div class="dashboard-message">';
 
 $user = current_user();
 if ($user)
 {
-    $html .= "<p>You are logged in as: $user->username</p>";
-    $html .= '<div><a href="' . WEB_ROOT . '/users/logout">Logout</a></div>';
+    $html .= '<h1>' . __('Welcome to the Digital Archive') . '</h1>';
+    $html .= '<p>' . __('You are logged in as <strong>%s</strong>', $user->username) . ' &nbsp;&nbsp; <a href="' . WEB_ROOT . '/users/logout">Logout</a></p>';
+    $html .= '<p>' . __('Statistics for this site are below. To see statistics for all organizations that you can search from this site, click ') . ' <a href="' . public_url('/find/contributors') . '">here.</a></p>';
 }
 else
 {
     $html .= '<p><a href="' . WEB_ROOT . '/users/login">Administrator Login</a></p>';
 }
+
+if (empty($stats))
+{
+    $html .= '</div>';
+}
+else
+{
+    $html .= '</div>';
+    $html .= $stats;
+}
+
 echo $html;
 echo foot();
 
