c867974ec4d64331fc01c183a10bd9473720439f
gsoules <gsoules@avantlogic.com>
2019-09-21 08:05:48 -0400

    Move Contributor stats to dashboard.

--- views/public/index/dashboard.php
+++ views/public/index/dashboard.php
@@ -2,38 +2,47 @@
 $pageTitle = __('Dashboard');
 echo head(array('bodyclass' => 'index primary-secondary', 'title' => $pageTitle));
 
-$stats = '';
+$localStats = '';
+$sharedStats = '';
+
 $useElasticsearch = AvantSearch::useElasticsearch();
 if ($useElasticsearch)
 {
-    $indexName = AvantElasticsearch::getNameOfLocalIndex();
-    $stats = AvantElasticsearch::generateContributorStatistics($indexName);
+    $sharedIndexName = AvantElasticsearch::getNameOfLocalIndex();
+    $localStats = AvantElasticsearch::generateContributorStatistics($sharedIndexName);
+    $localIndexName = AvantElasticsearch::getNameOfSharedIndex();
+    $sharedStats = AvantElasticsearch::generateContributorStatistics($localIndexName);
 }
 
 $html = '<div class="dashboard-message">';
 
+$html .= '<h1>' . __('Welcome to the Digital Archive') . '</h1>';
+
 $user = current_user();
 if ($user)
 {
-    $html .= '<h1>' . __('Welcome to the Digital Archive') . '</h1>';
     $html .= '<p>' . __('You are logged in as <strong>%s</strong>', $user->username) . ' &nbsp;&nbsp; <a href="' . WEB_ROOT . '/users/logout">Logout</a></p>';
-    $html .= '<p>' . __('Statistics for this site are below. To see statistics for all organizations that you can search from this site, click ') . ' <a href="' . public_url('/find/contributors') . '">here.</a></p>';
 }
 else
 {
     $html .= '<p><a href="' . WEB_ROOT . '/users/login">Administrator Login</a></p>';
 }
 
-if (empty($stats))
+$html .= '<p>' . __('The following organizations have made their collections searchable from this site:') . '</p>';
+
+if (!empty($sharedStats))
 {
-    $html .= '</div>';
-}
-else
-{
-    $html .= '</div>';
-    $html .= $stats;
+    $html .= $sharedStats;
 }
 
+if ($user && !empty($localStats))
+{
+    $html .= '<br/>';
+    $html .= '<p>' . __('Statistics for this site (including non-public items) appear below:') . '</p>';
+    $html .= $localStats;
+}
+
+$html .= '</div>';
 echo $html;
 echo foot();
 
