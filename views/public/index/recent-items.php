f3a7a044f2bc9698daa9a6afc97d0b09e0b8d75f
gsoules <gsoules@avantlogic.com>
2020-07-09 12:01:27 -0400

    Move static methods used by multiple plugins from AvantAdmin to AvantCommon,

--- views/public/index/recent-items.php
+++ views/public/index/recent-items.php
@@ -1,9 +1,9 @@
 <?php
 $pageTitle = __('Recent Items');
 echo head(array('bodyclass' => 'index primary-secondary', 'title' => $pageTitle));
-$recentlyViewedItems = AvantAdmin::getRecentlyViewedItems();
+$recentlyViewedItems = AvantCommon::getRecentlyViewedItems();
 
-echo AvantAdmin::emitRecentlyViewedItems($recentlyViewedItems);
+echo AvantCommon::emitRecentlyViewedItems($recentlyViewedItems);
 
 echo foot();
 
