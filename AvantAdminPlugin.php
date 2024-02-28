371f0e25a82c63a66c462fc0a26dee2874abc0fc
George Soules <gsoules@avantlogic.com>
2024-02-28 11:55:09 -0500

    Remove test for useElasticsearch to avoid runtime error.
    The code used to test for AvantSearch::useElasticsearch(), but if you bumped the version on the AvantSearch plugin, the code would fail because the function was undefined.

--- AvantAdminPlugin.php
+++ AvantAdminPlugin.php
@@ -27,8 +27,7 @@ class AvantAdminPlugin extends Omeka_Plugin_AbstractPlugin
 
     public static function allowAddItem()
     {
-        $allowAddItem = true;
-        if (plugin_is_active('AvantElasticsearch') && AvantSearch::useElasticsearch())
+        if (plugin_is_active('AvantElasticsearch'))
         {
             $allowAddItem = (bool)get_option(ElasticsearchConfig::OPTION_ES_LOCAL) == true;
         }
